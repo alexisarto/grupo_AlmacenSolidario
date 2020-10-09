ef3c438e8325de12b96613ae51bee691391deaaa
alexisarto <alexis.artopoulos@alumnos.pestalozzi.edu.ar>
2020-10-09 17:51:35 -0300

    primer commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/registro', function(req, res, next) {
+  res.render('register');
+});
+
+router.get('/carrito', function(req, res, next) {
+  res.render('productCart');
+});
+
+router.get('/detalle-producto', function(req, res, next) {
+  res.render('productDetail');
+});
+
+router.get('/agregar-producto', function(req, res, next) {
+  res.render('productAdd');
+});
+
+module.exports = router;
