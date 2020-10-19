38c33dccfe4f7b9eb436df28603e14a9d8e4776e
alexisarto <alexis.artopoulos@alumnos.pestalozzi.edu.ar>
2020-10-19 14:42:27 -0300

    commit de prueba

--- routes/index.js
+++ routes/index.js
@@ -22,21 +22,19 @@ router.get('/agregar-producto', function(req, res, next) {
   res.render('productAdd');
 });
 
-<<<<<<< HEAD
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
 
 router.get('/resetear-contrasenia', function(req, res, next) {
-  res.render('passwordReset');
-=======
+  res.render('passwordReset')});
+
 router.get('/home', function(req, res, next) {
   res.render('home');
 });
 
 router.get('/instituciones', function(req, res, next) {
   res.render('intituciones');
->>>>>>> f99a0efc55de7c86c6498e6881b179dda49328ae
 });
 
 module.exports = router;
