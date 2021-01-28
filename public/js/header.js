12528666a53bb278b7999e631ef05fbca9ff1efd
Timibar3 <glocoselli@gmail.com>
2021-01-28 05:29:11 -0300

    responsive header 70%

---
+++ public/js/header.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function(){
+    console.log("hola")
+    let burger = document.querySelector(".icon-burger");
+    var links = document.querySelector(".links")
+
+    burger.addEventListener('click', function(){
+        links.classList.toggle("show")
+
+    })
+
+})
